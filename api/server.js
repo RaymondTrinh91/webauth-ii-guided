1eb9c296e8419683517db7bcf0c321f14f77a7ee
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-12-17 14:16:09 -0800

    mvp

--- api/server.js
+++ api/server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(sessions)
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knex = require('../database/dbConfig.js')
 
 const server = express();
+
+const sessionConfiguration = {
+  name: 'sid',
+  secret: 'keep it secret, keep it safe!',
+  saveUninitialized: true,
+  resave: false,
+
+  store: new KnexSessionStore({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 60, //default is 1000 * 60 * 10
+    tablename: 'sessions', //default
+    sidfieldname: 'sid', //default
+  }),
+
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  }
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionConfiguration))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
